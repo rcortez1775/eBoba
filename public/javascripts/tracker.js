5b9dfff1c84671b4fe32963eb84a1323d5c7a041
Robert Cortez <Nuhuhfoo@gmail.com>
2019-03-17 16:20:37 -0700

    updated the menu function using a ternary operator

--- public/javascripts/tracker.js
+++ public/javascripts/tracker.js
@@ -1,11 +1,6 @@
-/* Open when someone clicks on the span element */
-function openNav() {
-    document.getElementById("myNav").style.width = "100%";
-}
-
-/* Close when someone clicks on the "x" symbol inside the overlay */
-function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
+function displayNav() {
+    var nav = document.getElementById("myNav");
+    nav.style.width = nav.style.width == '100%' ? '0%' : '100%';
 }
 
 function foodFunction() {
