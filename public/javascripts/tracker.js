ebe165a79a398b23f92a926e01ab80604ac27827
Robert Cortez <Nuhuhfoo@gmail.com>
2019-03-18 21:39:39 -0700

    used single function for all activity buttons

--- public/javascripts/tracker.js
+++ public/javascripts/tracker.js
@@ -8,7 +8,7 @@ function myFunction() {
     x.style.display = x.style.display === 'none' ? 'block' : 'none';
 }
 
-function foodFunction() {
+function activityFunction(title) {
     var currentDate = new Date(),
         day = currentDate.getDate(),
         month = currentDate.getMonth() + 1,
@@ -34,100 +34,7 @@ function foodFunction() {
     var cell1 = row.insertCell(0);
     var cell2 = row.insertCell(1);
     var cell3 = row.insertCell(2);
-    cell1.innerHTML = "Food";
-    cell2.innerHTML = hours + ":" + minutes + " " + suffix;
-    cell3.innerHTML = month + "/" + day + "/" + year;
-}
-
-function bathFunction() {
-    var currentDate = new Date(),
-        day = currentDate.getDate(),
-        month = currentDate.getMonth() + 1,
-        year = currentDate.getFullYear();
-
-    var currentTime = new Date(),
-        hours = currentTime.getHours(),
-        minutes = currentTime.getMinutes();
-    if (minutes < 10) {
-        minutes = "0" + minutes;
-    }
-
-    var suffix = "AM";
-    if (hours >= 12) {
-        suffix = "PM";
-        hours = hours - 12;
-    }
-    if (hours == 0) {
-        hours = 12;
-    }
-    var table = document.getElementById("myTable");
-    var row = table.insertRow(1);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
-    cell1.innerHTML = "Bath";
-    cell2.innerHTML = hours + ":" + minutes + " " + suffix;
-    cell3.innerHTML = month + "/" + day + "/" + year;
-}
-
-function walkFunction() {
-    var currentDate = new Date(),
-        day = currentDate.getDate(),
-        month = currentDate.getMonth() + 1,
-        year = currentDate.getFullYear();
-
-    var currentTime = new Date(),
-        hours = currentTime.getHours(),
-        minutes = currentTime.getMinutes();
-    if (minutes < 10) {
-        minutes = "0" + minutes;
-    }
-
-    var suffix = "AM";
-    if (hours >= 12) {
-        suffix = "PM";
-        hours = hours - 12;
-    }
-    if (hours == 0) {
-        hours = 12;
-    }
-    var table = document.getElementById("myTable");
-    var row = table.insertRow(1);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
-    cell1.innerHTML = "Walk";
-    cell2.innerHTML = hours + ":" + minutes + " " + suffix;
-    cell3.innerHTML = month + "/" + day + "/" + year;
-}
-
-function medFunction() {
-    var currentDate = new Date(),
-        day = currentDate.getDate(),
-        month = currentDate.getMonth() + 1,
-        year = currentDate.getFullYear();
-
-    var currentTime = new Date(),
-        hours = currentTime.getHours(),
-        minutes = currentTime.getMinutes();
-    if (minutes < 10) {
-        minutes = "0" + minutes;
-    }
-
-    var suffix = "AM";
-    if (hours >= 12) {
-        suffix = "PM";
-        hours = hours - 12;
-    }
-    if (hours == 0) {
-        hours = 12;
-    }
-    var table = document.getElementById("myTable");
-    var row = table.insertRow(1);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
-    cell1.innerHTML = "Meds";
+    cell1.innerHTML = title;
     cell2.innerHTML = hours + ":" + minutes + " " + suffix;
     cell3.innerHTML = month + "/" + day + "/" + year;
 }
